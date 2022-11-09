99922cf3903a19bf6a33f16bd958d4d305ccda28
Iryna Hridnova <diukovairyna@ukr.net>
2022-11-09 18:34:12 +0200

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+btnStart.addEventListener('click', () => {
+  interval = setInterval(() => {
+   document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(interval);
+    btnStart.disabled = false;
+});
+
+
+
+
 
